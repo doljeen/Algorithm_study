a06327b6a02974d7ca1c7258dede56ea214a7435
seokko <156649520+doljeen@users.noreply.github.com>
2026-02-02 12:16:28 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 133 ms, Memory: 33,392 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = 10;
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int N = sc.nextInt();
+			List<Integer> actArr = new LinkedList<>();
+			for(int i =0; i < N; i++) {
+				actArr.add(sc.nextInt());
+			}
+			int C = sc.nextInt();
+			for(int i = 0; i < C; i++) {
+				String a = sc.next();
+				int x = sc.nextInt();
+				int y = sc.nextInt();
+				if(a.equals("D")) {
+					for(int j = 0; j < y; j++) {
+						actArr.remove(x);
+					}
+				}else {
+					for(int j = 0; j < y; j++) {
+						actArr.add(x++, sc.nextInt());
+					}
+				}
+			}
+			
+			System.out.print("#" + test_case + " ");
+			for(int i = 0; i < 10; i++) {
+				System.out.print(actArr.get(i) + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
