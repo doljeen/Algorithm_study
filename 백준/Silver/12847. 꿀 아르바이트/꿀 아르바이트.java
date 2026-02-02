0d724e073bc66b8033ca2ee102bbdbf1d3add3a9
seokko <156649520+doljeen@users.noreply.github.com>
2026-02-02 17:28:27 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 676 ms, Memory: 108280 KB -BaekjoonHub

--- 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -1,32 +1,29 @@
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		List<Integer> arr = new ArrayList<>();
-		for(int i = 0; i < N; i++) {
-			arr.add(sc.nextInt());
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		long []sum = new long[n+1];
+		for(int i=1; i<=n; i++)
+		{
+			int work = sc.nextInt();
+			sum[i] = work+sum[i-1];
 		}
 		long max = 0;
-		long result = 0;
-		for(int i = 0; i < N; i++) {
-			if(i < M) {
-				result += arr.get(i);
-				max = Math.max(max, result);
-			}else {
-				result += arr.get(i);
-				result -= arr.get(i - M);
-				max = Math.max(max, result);
+		for(int i=m; i<=n; i++)
+		{
+			long now = sum[i]-sum[i-m];
+			if(now>max)
+			{
+				max = now;
 			}
-			
 		}
 		System.out.println(max);
+		
 	}
 
 }
