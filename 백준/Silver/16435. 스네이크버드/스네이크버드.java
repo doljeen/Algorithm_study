82229d55eefbb0d849d514bac7f14097f16ed538
seokko <156649520+doljeen@users.noreply.github.com>
2026-02-10 15:03:31 +0900

    [Silver V] Title: 스네이크버드, Time: 224 ms, Memory: 20892 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int L = sc.nextInt();
+		int start = 0;
+		int cnt = 0;
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		for(int i = 0; i < N; i++) {
+			if(arr[i] <= L) {
+				L++;
+			}else {
+				break;
+			}
+		}
+		System.out.println(L);
+	}
+
+}
