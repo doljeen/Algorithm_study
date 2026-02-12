f4ce0a8dcd3c9e1c25340f3ff27ca909c6c69354
seokko <156649520+doljeen@users.noreply.github.com>
2026-02-12 15:58:49 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1408 ms, Memory: 138108 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,68 +1,59 @@
-import java.lang.reflect.Array;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb= new StringBuilder();
-		// 전체 카드 개수
-		int n = sc.nextInt();
-		// 카드 받기
-		int c[] = new int[n];
-		
-		for(int i=0; i<n; i++)
-		{
-			c[i] = sc.nextInt();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] ans = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			ans[i] = Integer.parseInt(st.nextToken());
 		}
-		//이진 탐색 정렬
-		Arrays.sort(c);
-		// 몇 개인지 세어야할 수의 개수
-		int m = sc.nextInt();
-
-		for(int k=0; k<m ; k++)
-		{
-			// 몇개인지 찾아야할 숫자
-			int f = sc.nextInt();
-			int li=0;
-			int lj=c.length;
-			int ui=0;
-			int uj=c.length;
-			
-			while(li<lj)
-			{
-				int mid = (li+lj)/2;
-				if(c[mid] < f)
-				{
-					li = mid+1;
-				}
-				else if(c[mid]>= f)
-				{
-					lj = mid;
-				}
-			}
-		
-			while(ui<uj)
-			{
-				int mid = (ui+uj)/2;
-				if(c[mid] <= f)
-				{
-					ui = mid+1;
-				}
-				else if(c[mid]> f)
-				{
-					uj = mid;
-				}
-			}
-			
-			sb.append(ui-li).append(" ");
-			
+		int M = Integer.parseInt(br.readLine());
+		int[] arr = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < M; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		StringBuilder sb = new StringBuilder();
+		Arrays.sort(ans);
+		for(int i = 0; i < M; i++) {
+			int upper = 0;
+			int lower = 0;
+			upper = getUpper(ans, arr[i]);
+			lower = getLower(ans, arr[i]);
+			sb.append((upper - lower) + " ");
 		}
 		System.out.println(sb);
 		
-		
 	}
-
+	
+	public static int getUpper(int[] ans, int target) {
+		int s = 0;
+		int e = ans.length;
+		while(s < e) {
+			int m = (s+e)/2;
+			if(ans[m] <= target) {
+				s = m+1;
+			}else e = m;
+		}
+		return e;
+	}
+	
+	public static int getLower(int[] ans, int target) {
+		int s = 0;
+		int e = ans.length;
+		while(s < e) {
+			int m = (s+e)/2;
+			if(ans[m] < target) {
+				s = m+1;
+			}else e = m;
+		}
+		return e;
+	}
 }
