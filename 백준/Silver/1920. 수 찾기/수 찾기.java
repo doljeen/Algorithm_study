54c9ec13ad83a3311c6b5b9ad970aaa4938c679c
seokko <156649520+doljeen@users.noreply.github.com>
2026-02-12 13:47:20 +0900

    [Silver IV] Title: 수 찾기, Time: 1576 ms, Memory: 116520 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,60 +1,37 @@
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		// 전체 숫자 개수
 		int n = sc.nextInt();
 		// 2^31승이면 10^9승 넘어가니까.. 범위 크게..
-		long a[] = new long[n];
+		HashMap<String, Integer> hmap =new HashMap<>();
 		
 		for(int i=0; i<n ; i++)
 		{
-			a[i] = sc.nextInt();
+			String tmp = sc.next();
+			hmap.put(tmp, 1);
 		}
 		// 찾아야 하는 수의 개수
 		int fn = sc.nextInt();
-		// 찾아야 하는 수
-		long f[] = new long[fn];
 
 
 		for(int i=0; i<fn; i++)
 		{
-			f[i] = sc.nextInt();
-		}
-		Arrays.sort(a);
-
-		for(int k=0; k<fn; k++)
-		{
-			int i=0; 
-			int j=a.length-1;
-			int isfind = 0;
-			while(i<=j)
+			String find = sc.next();
+			if(hmap.containsKey(find))
 			{
-				int mid = (i+j)/2;
-				if(a[mid] == f[k])
-				{
-					System.out.println(1);
-					isfind = 1;
-					break;
-				}
-				if(a[mid] <f[k])
-				{
-					i = mid+1;
-				}
-				if(a[mid] > f[k])
-				{
-					j= mid-1;
-				}
-				
+				System.out.println(1);
 			}
-			if(isfind == 0)
+			else
 			{
 				System.out.println(0);
 			}
 		}
-	
-		
 	}
+
 }
