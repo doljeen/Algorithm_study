9f817b46c9117766b3e18db30cf9ff9eb6629deb
seokko <156649520+doljeen@users.noreply.github.com>
2026-02-13 14:10:48 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 104 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] arr;
+	static String result;
+	static int N;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		result = "Hing";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		dfs(0, 0);
+		System.out.println(result);
+	}
+	
+	public static void dfs(int start, int end) {
+		if(start >= N || end >= N || arr[start][end] == 0) return;
+		if(start == N-1 && end == N-1) {
+			result = "HaruHaru";
+			return;
+		}
+		int curValue = arr[start][end];
+		int row = start + curValue;
+		int col = end + curValue;
+		dfs(row, end);
+		dfs(start, col);
+	}
+
+}
