f29e54472057c338a1c458ecb87a2adc7d821576
seokko <156649520+doljeen@users.noreply.github.com>
2026-02-19 16:11:42 +0900

    [Silver III] Title: N과 M (4), Time: 540 ms, Memory: 27796 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int N;
+	public static int M;
+	private static int[] numbers;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		numbers = new int[M];
+		Permu(0, 1);
+	}
+	
+	public static void Permu(int cnt, int idx) {
+		if(cnt == M) {
+			for(int i =0; i < M; i++) {
+				System.out.print(numbers[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i = idx; i <= N; i++) {
+			numbers[cnt] = i;
+			Permu(cnt+1, i);
+		}
+	}
+}
