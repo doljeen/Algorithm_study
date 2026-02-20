da854226100550a2f26a9636bbd991547f956161
seokko <156649520+doljeen@users.noreply.github.com>
2026-02-20 14:12:32 +0900

    [Silver II] Title: 섬의 개수, Time: 144 ms, Memory: 15992 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,35 +1,37 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int h, w;
+	public static int w, h;
 	public static int[][] arr;
 	public static boolean[][] visited;
 	public static int[] dx = {0, 1, 0, -1, 1, -1, 1, -1};
 	public static int[] dy = {1, 0, -1, 0, 1, 1, -1, -1};
-	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while(true) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			h = Integer.parseInt(st.nextToken());
 			w = Integer.parseInt(st.nextToken());
-			arr = new int[w][h];
-			visited = new boolean[w][h];
+			h = Integer.parseInt(st.nextToken());
+			if(w == 0 && h == 0) break;
+			arr = new int[h][w];
+			visited = new boolean[h][w];
 			int count = 0;
-			if(h == 0 && w == 0) break;
-			for(int i = 0; i < w; i++) {
+			for(int i = 0; i < h; i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int j = 0; j < h; j++) {
+				for(int j = 0; j < w; j++) {
 					arr[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			for(int i = 0; i < w; i++) {
-				for(int j = 0; j < h; j++) {
+			for(int i = 0; i < h; i++) {
+				for(int j = 0; j < w; j++) {
 					if(arr[i][j] == 1 && !visited[i][j]) {
-						dfs(i, j);
+						bfs(i, j);
 						count++;
 					}
 				}
@@ -37,13 +39,22 @@ public class Main {
 			System.out.println(count);
 		}
 	}
-	public static void dfs(int row, int col) {
+	private static void bfs(int row, int col) {
+		Queue<int[]> queue = new ArrayDeque<>();
 		visited[row][col] = true;
-		for(int i = 0; i < 8; i++) {
-			int nr = row+dx[i];
-			int nc = col+dy[i];
-			if(nr >= w || nc >= h || nr < 0 || nc < 0 || visited[nr][nc] || arr[nr][nc] == 0) continue;
-			dfs(nr, nc);
+		queue.offer(new int[] {row, col});
+		while(!queue.isEmpty()) {
+			int[] current = queue.poll();
+			int curR = current[0];
+			int curC = current[1];
+			for(int i = 0; i < 8; i++) {
+				int nr = curR + dy[i];
+				int nc = curC + dx[i];
+				if(nr < 0 || nr >= h || nc < 0 || nc >= w || visited[nr][nc] || arr[nr][nc] == 0) continue;
+				queue.offer(new int[] {nr, nc});
+				visited[nr][nc] = true;
+			}
 		}
 	}
+
 }
