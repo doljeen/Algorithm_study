c127db42e39c11c8df96bab39e136adc49e43604
seokko <156649520+doljeen@users.noreply.github.com>
2026-02-23 17:49:43 +0900

    [Gold III] Title: 줄 세우기, Time: 680 ms, Memory: 50228 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,52 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		
+		// 순서 1 화살표 개수 세기
+		int[] arr = new int[N];
+		// 순서 2 화살표 개수 0개인거 큐에 추가하기 싫으면 하지않기
+		List<Integer>[] list = new ArrayList[N];
+		for (int i=0;i<N;i++) list[i] = new ArrayList<Integer>();
+		
+		// 순서 3 0개인거 빼면서 출력하면서 0개 도달하면 큐에 넣기싫으면 안넣기
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken())-1;
+			int B = Integer.parseInt(st.nextToken())-1;
+			list[A].add(B);
+			arr[B]++;
+		}
+		
+		Queue<Integer> queue = new ArrayDeque<>();
+		for(int i= 0; i < N; i++) {
+			if(arr[i] == 0) {
+				queue.add(i);
+			}
+		}
+		while(!queue.isEmpty()) {
+			int current = queue.poll();
+			System.out.print(current + 1 + " ");
+			for(int i = 0; i < list[current].size(); i++) {
+				arr[list[current].get(i)]--;				
+				if(arr[list[current].get(i)] == 0) 
+					queue.add(list[current].get(i));
+			}
+			
+		}
+	}
+}
