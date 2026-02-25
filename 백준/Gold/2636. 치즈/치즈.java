5f2c6eebb5b3b5aa603dee7aee89aa1e4606b3b7
seokko <156649520+doljeen@users.noreply.github.com>
2026-02-25 14:31:37 +0900

    [Gold IV] Title: 치즈, Time: 164 ms, Memory: 17176 KB -BaekjoonHub

---
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -0,0 +1,76 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int N, M, cnt;
+	public static int[][] arr;
+	public static boolean[][] visited;
+	public static int[] dx = {1, -1, 0, 0};
+	public static int[] dy = {0, 0, -1, 1};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		cnt = 0;
+		int min = Integer.MAX_VALUE;
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		int stop = 1;
+		while(stop > 0) {
+			stop = 0;
+			visited = new boolean[N][M];
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < M; j++) {
+					if(arr[i][j] == 1) {
+						stop++;
+					}
+				}
+			}
+			int tmp = bfs();
+			if(min > tmp)min = tmp;
+		}
+		System.out.println(cnt-1 + " " + min);
+	}
+	public static int bfs() {
+		cnt++;
+		int result = 0;
+		visited[0][0] = true;
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[] {0,0});
+		while(!queue.isEmpty()) {
+			int[] current = queue.poll();
+			int curR = current[0];
+			int curC = current[1];
+			for(int i = 0; i < 4; i++) {
+				int nr = curR + dx[i];
+				int nc = curC + dy[i];
+				if(nr < 0 || nr >= N || nc < 0 || nc >= M || visited[nr][nc]) continue;
+				if(arr[nr][nc] == 1) {
+					visited[nr][nc] = true;
+					arr[nr][nc] = 0;
+					result++;
+				} else {
+					queue.offer(new int[] {nr, nc});
+					visited[nr][nc] = true;
+				}
+				
+			}
+		}
+		if(result > 0) {
+			return result;			
+		}else {
+			return Integer.MAX_VALUE;
+		}
+	}
+}
