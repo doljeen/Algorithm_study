5adba77612eeaa091f718b4e0046dac94f262926
seokko <156649520+doljeen@users.noreply.github.com>
2026-03-10 12:12:07 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 644 ms, Memory: 53752 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+    static int E;
+    static Node[] vertices;
+    static int[] parent;
+
+
+    static int findParent(int x) {
+        if (x == parent[x]) return x;
+        return parent[x] = findParent(parent[x]);
+    }
+
+    static boolean union(int a, int b) {
+        a = findParent(a);
+        b = findParent(b);
+
+        if (a == b) return false;
+
+        if (a < b) parent[b] = a;
+        else parent[a] = b;
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        vertices = new Node[E];
+        parent = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) parent[i] = i;
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            vertices[i] = new Node(u, v, weight);
+        }
+
+        Arrays.sort(vertices, (a, b) -> Integer.compare(a.weight, b.weight));
+
+        long maxWeight = 0;
+
+        for (int i = 0; i < E; i++) {
+            int a = vertices[i].u;
+            int b = vertices[i].v;
+            if (union(a, b)) {
+                maxWeight += vertices[i].weight;
+            }
+        }
+        
+        System.out.println(maxWeight);
+    }
+
+    static class Node {
+        int u;
+        int v;
+        int weight;
+        Node(int u, int v, int weight) {
+            this.u = u;
+            this.v = v;
+            this.weight = weight;
+        }
+    }
+}
